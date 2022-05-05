4b52623cc56e33b0f0eb83ce4f43858b3b49df5f
philimonovoleg <philimonovoleg@gmail.com>
2022-05-05 21:52:33 +0300

    Add project files.

---
+++ Labyrinth/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Labyrinth
+{
+    public interface IRendererObject
+    {
+        void Render();
+    }
+
+    public class Labyrinth : IRendererObject
+    {
+        private readonly int _columns, _rows;
+        private readonly char[,] _grid;
+
+        public Labyrinth(int columns = 10, int rows = 10)
+        {
+            _grid = new char[columns, rows];
+        }
+
+        public void Render()
+        {
+            Console.SetCursorPosition(0, 0);
+            for(int column = 0; column < _columns; column++)
+            {
+                for(int row = 0; row < _rows; row++)
+                {
+                    Console.Write(_grid[column, row]);
+                }
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
